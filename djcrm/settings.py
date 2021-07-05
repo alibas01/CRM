06919a8bcced7b2ca756a18249b40f2bcc58e3ca
Ali Bas <alibas01@gmail.com>
2021-07-04 22:11:12 -0400

    .env file, secret key

--- djcrm/settings.py
+++ djcrm/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import environ
+import os
+# Initialise environment variables
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'fip6ak5a$-@i9&tcnh#&11t3v*m0@i+iy_jxs_+pb-24(%%g@b'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
