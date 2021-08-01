5ea449b8e50c87af98bc817407aee7afb35349fa
Ali Bas <alibas01@gmail.com>
2021-08-01 15:09:50 -0400

    Lead List, Lead Details

--- leads/views.py
+++ leads/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Lead
 
-def home_page(request):
+def lead_list(request):
     #return HttpResponse('home.html')
-    return render(request, 'second_page.html', {})
+    leads = Lead.objects.all()
+    context = {"leads":leads}
+    return render(request, 'lead_list.html', context)
+
+def lead_detail(request, pk):
+    print(pk)
+    # return HttpResponse('detail.html')
+    lead = Lead.objects.get(id=pk)
+    print(lead)
+    context = {"lead":lead}
+    return render(request, 'lead_detail.html', context)
