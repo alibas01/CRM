f82c7a1d1fb8218ff9f04230bf882fe10b94477d
Ali Bas <alibas01@gmail.com>
2021-08-02 00:40:09 -0400

    update all html and url with namespace

--- leads/urls.py
+++ leads/urls.py
@@ -4,9 +4,9 @@ from .views import *
 app_name = 'leads'
 
 urlpatterns = [
-    path('', lead_list, name='home'),
-    path('<int:pk>/', lead_detail, name='detail'),
-    path('<int:pk>/update', lead_update, name='update'),
-    path('<int:pk>/delete', lead_delete, name='delete'),
-    path('create/', lead_create, name='create')
+    path('', lead_list, name='lead-list'),
+    path('<int:pk>/', lead_detail, name='lead-detail'),
+    path('<int:pk>/update', lead_update, name='lead-update'),
+    path('<int:pk>/delete', lead_delete, name='lead-delete'),
+    path('create/', lead_create, name='lead-create')
 ]
